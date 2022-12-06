e92149eb55b60c423fe2d14b6087174b7c610210
José García Borge <adrian23gb@hotmail.com>
2022-12-05 19:03:29 -0600

    Agregar productos. Por verificar

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -11,31 +11,31 @@ use Validator;
 class ProductoController extends Controller
 {
 
-    public function presentacion(string $nombre)
+    public function getIdPresentacion(string $nombre)
     {
         if (!Presentacion::where('Nombre', '=', $nombre)->first()) {
             $nuevoP = Presentacion::create([
                 'Nombre' => $nombre
             ]);
-            return $nuevoP->id;
+            return $nuevoP[0]->id_presentacion;
         }
         $nuevoP = Presentacion::where('Nombre', '=', $nombre)->get();
-       echo($nuevoP->all());
-        return $nuevoP->id_presentacion;
+        return $nuevoP[0]->id_presentacion;
     }
 
-    public function unidad(string $unidad)
+    public function getIdunidad(string $unidad)
     {
         if (!Unidad_Medida::where('Nombre', '=', $unidad)->first()) {
             $nuevaU = Unidad_Medida::create([
                 'Nombre' => $unidad
             ]);
             
-            return $nuevaU->id;
+            return $nuevaU[0]->id_unidad;
         }
 
-        $laUnidad = Unidad_Medida::where('Nombre','=', $unidad);
-        return $laUnidad->id;
+        $laUnidad = Unidad_Medida::where('Nombre','=', $unidad)->get();
+        //echo $laUnidad[0]->id_unidad;
+        return $laUnidad[0]->id_unidad;
     }
 
     public function nuevoproducto(Request $request)
@@ -55,16 +55,15 @@ class ProductoController extends Controller
             return response()->json(['mensage', $validar->erros()]);
         }
 
-        $idpresentacion = self::presentacion($request->presentacion);
-        $idunidad = self::unidad($request->unidad);
+        //$idpresentacion = self::presentacion($request->presentacion);
+        //$idunidad = self::unidad($request->unidad);
 
         Producto::create([
             'Codigo_producto' => $request->codigoproducto,
             'Nombre' => $request->nombre,
-            'id_presentacion' => $idpresentacion,
-            'id_unidad' => $idunidad,
+            'id_presentacion' => self::getIdPresentacion($request->presentacion),
+            'id_unidad' => self::getIdunidad($request->unidad),
             'Precio_venta' =>$request->precioventa,
-
         ]);
     }
 
